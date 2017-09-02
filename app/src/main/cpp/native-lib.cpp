03bd02c37e6dde03b020a8c6fa82673ee7909e89
leon <597765252@qq.com>
2017-09-02 21:09:21 +0800

    commit

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,5 +1,10 @@
 #include <jni.h>
 #include <string>
+#include <android/log.h>
+
+#define LOGI(FORMAT, ...) __android_log_print(ANDROID_LOG_INFO,"TAG",FORMAT,##__VA_ARGS__);
+#define LOGE(FORMAT, ...) __android_log_print(ANDROID_LOG_ERROR,"TAG",FORMAT,##__VA_ARGS__);
+
 
 extern "C"
 JNIEXPORT jstring JNICALL
@@ -7,5 +12,6 @@ Java_leon_training_ndk_Client_getStringFromCPlus(
         JNIEnv *env,
         jobject /* this */) {
     std::string hello = "Hello from C++";
+    LOGE("getStringFromCPlus()");
     return env->NewStringUTF(hello.c_str());
 }
